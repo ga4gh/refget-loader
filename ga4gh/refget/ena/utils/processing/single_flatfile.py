55b0db3cc52916a0734b1ae0579eaa500ae260a1
Jeremy Adams <jeremy.adams@ga4gh.org>
2019-10-01 09:14:40 -0400

    added method to write process cmd and bsub files

--- ga4gh/refget/ena/utils/processing/single_flatfile.py
+++ ga4gh/refget/ena/utils/processing/single_flatfile.py
@@ -1,15 +1,34 @@
 import json
 import os
 from ga4gh.refget.ena.functions.time import timestamp
+from ga4gh.refget.ena.resources.get_resource import parse_settings_ini
+
+def write_cmd_and_bsub(cmd, cmd_dir, cmd_name):
+    cmd_file = os.path.join(cmd_dir, cmd_name + ".sh")
+    bsub_file = os.path.join(cmd_dir, cmd_name + ".bsub")
+    bsub = 'bsub "{}"'.format(cmd_file)
+    open(cmd_file, "w").write(cmd + "\n")
+    open(bsub_file, "w").write(bsub + "\n")
+
+def write_process_cmd_and_bsub(subdir, file_path, process_id, cmd_dir):
+    perl_script = parse_settings_ini()["refget_ena_settings"]["perl_script"]
+    cmd_template = "{} --store-path {} --file-path {} --process-id {}"
+    cmd = cmd_template.format(perl_script, subdir, file_path, process_id)
+    write_cmd_and_bsub(cmd, cmd_dir, "process")
+
+def make_upload_command():
+    pass
 
 def process_single_flatfile(processing_dir, accession, url):
 
     url_basename = os.path.basename(url)
-    subdir = os.path.join(processing_dir, "files", url_basename[:2], 
-        url_basename.split(".")[0]
-    )
-    if not os.path.exists(subdir):
-        os.makedirs(subdir)
+    url_id = url_basename.split(".")[0]
+    subdir = os.path.join(processing_dir, "files", url_basename[:2], url_id)
+    cmd_dir = os.path.join(subdir, "cmd")
+    log_dir = os.path.join(subdir, "log")
+    for d in [subdir, cmd_dir, log_dir]:
+        if not os.path.exists(d):
+            os.makedirs(d)
 
     status_fp = os.path.join(subdir, "status.txt")
     status_dict = {
@@ -21,16 +40,18 @@ def process_single_flatfile(processing_dir, accession, url):
     }
 
     try:
-        symlink_from = url.replace("ftp://ftp.ebi.ac.uk", "/nfs/ftp")
-        symlink_to = os.path.join(subdir, url_basename)
+        dat_orig = url.replace("ftp://ftp.ebi.ac.uk", "/nfs/ftp")
+        dat_link = os.path.join(subdir, url_basename)
 
-        if not os.path.exists(symlink_from):
+        if not os.path.exists(dat_orig):
             raise Exception(
                 "input .dat file could not be located at the path: " 
-                + symlink_from
+                + dat_orig
             )
+        os.symlink(dat_orig, dat_link)
 
-        os.symlink(symlink_from, symlink_to)
+        
+        bsub_template = ""
         
     except Exception as e:
         status_dict["status"] = "Failed"
